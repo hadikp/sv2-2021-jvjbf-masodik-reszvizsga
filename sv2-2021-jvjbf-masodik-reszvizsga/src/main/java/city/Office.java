de4c7955929f99d8e5ded4207f5cd9a28d30199b
hadikp <hp20001025@gmail.com>
2021-12-13 16:54:42 +0100

    office

--- sv2-2021-jvjbf-masodik-reszvizsga/src/main/java/city/Office.java
+++ sv2-2021-jvjbf-masodik-reszvizsga/src/main/java/city/Office.java
@@ -1,6 +1,35 @@
 package city;
 
-public class Office {
+public class Office extends Building {
 
     private String company;
+    private int numberOfTablesPerLevel;
+
+    public Office (int area, int levels, Address address, String company, int numberOfTablesPerLevel) {
+        super(area, levels, address);
+        /*if (getFullArea() >= (numberOfTablesPerLevel * 2)){
+            throw new IllegalArgumentException("Too many tables!");
+        }*/
+        this.company = company;
+        this.numberOfTablesPerLevel = numberOfTablesPerLevel;
+    }
+
+    public Office(int area, Address address, String company, int numberOfTablesPerLevel) {
+        super(area, address);
+        this.company = company;
+        this.numberOfTablesPerLevel = numberOfTablesPerLevel;
+    }
+
+    @Override
+    public int calculateNumberOfPeopleCanFit() {
+        return numberOfTablesPerLevel * (getLevels() - 1);
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public int getNumberOfTablesPerLevel() {
+        return numberOfTablesPerLevel;
+    }
 }
